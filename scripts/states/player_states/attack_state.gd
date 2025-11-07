f7844c25ebc321f0d88e8b3507d10bd52bfd6e79
aanastassatos <aanastassatos1@comcast.net>
2025-11-06 22:50:47 -0700

    Fixed Animation for to prevent body from staying rotated

--- scripts/states/player_states/attack_state.gd
+++ scripts/states/player_states/attack_state.gd
@@ -2,6 +2,8 @@ class_name AttackState
 extends PlayerState
 
 @export var launched_state : PlayerState 
+@export var hit_state : PlayerState
+@export var miss_state : PlayerState 
 @export var linger_duration : float = 0.6
 @export var slow_time : bool = false
 
@@ -10,23 +12,43 @@ var elapsed : float = 0.0
 
 var done_highlight : bool = false
 
+var pending_next_state : PlayerState = null
+
 func enter() -> void:
 	super()
+	if not EventBus.enemy_hit.is_connected(_on_enemy_hit):
+		EventBus.enemy_hit.connect(_on_enemy_hit)
+	
+	if not EventBus.enemy_missed.is_connected(_on_enemy_missed):
+		EventBus.enemy_missed.connect(_on_enemy_missed)
 	elapsed = 0.0
+	pending_next_state = null
 	done_highlight = false
 	parent.change_time_scale(slow_time_scale)
 	parent.animation_player.speed_scale = 1.0/parent.current_time_scale
 	doDiceHighlightAsync()
 
+func exit() -> void:
+	parent.animation_player.speed_scale = 1.0
+	parent.change_time_scale(1.0)
+	if EventBus.enemy_hit.is_connected(_on_enemy_hit):
+		EventBus.enemy_hit.disconnect(_on_enemy_hit)
+	
+	if EventBus.enemy_missed.is_connected(_on_enemy_missed):
+		EventBus.enemy_missed.disconnect(_on_enemy_missed)
+
+func _on_enemy_hit(enemy : Enemy) -> void:
+	pending_next_state = hit_state
+
+func _on_enemy_missed(enemy : Enemy) -> void:
+	print("enemy missed")
+	pending_next_state = miss_state
+
 func doProcess(delta: float) -> PlayerState:
 	elapsed += delta
 	if elapsed >= linger_duration*parent.current_time_scale and done_highlight:
 		return launched_state
-	return null
-
-func exit() -> void:
-	parent.animation_player.speed_scale = 1.0
-	parent.change_time_scale(1.0)
+	return pending_next_state
 
 func doDiceHighlightAsync() -> void:
 	pass
